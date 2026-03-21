7d392656c507467a3db8bab45d41f1b70258ba67
zndayo <cantabaptiste@outlook.com>
2026-03-21 11:07:47 +0100

    Update mathlafd.py
    i would add this to the file what is your opinion

--- mathlafd.py
+++ mathlafd.py
@@ -4,7 +4,9 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
-
+# I'd add this to the file 
+#if __string__ == '__main__':
+#   unittest.main()
 
 if __name__ == '__main__':
     unittest.main()
